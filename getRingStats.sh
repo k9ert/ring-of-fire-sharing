dd9c44e01b05f913cf65ce19fd2d9a81d5915fb9
Lightning Pi <lightning@raspberrypi>
2021-04-08 16:52:44 +0200

    Expand list of peers to whole ring

--- getRingStats.sh
+++ getRingStats.sh
@@ -6,8 +6,16 @@ echo "\"feeReport\":${feeReport}"
 
 jq -c '.peers[]' ringOfFireConfig.json | while read peer; do
   peer=$(echo "$peer" | tr -d '"')
-  peerInfo=$(lncli listchannels --peer "$peer" | jq -r '{ chan_id: .channels[].chan_id, local_balance: .channels[].local_balance, remote_balance: .channels[].remote_balance }')
-  echo ",\"${peer}\":${peerInfo}"
+  node_id=$(echo "$peer" | cut -f1 -d@)
+  peerInfo=$(lncli listchannels --peer "$node_id" | jq -r '{ chan_id: .channels[].chan_id, local_balance: .channels[].local_balance, remote_balance: .channels[].remote_balance, active: .channels[].active }')
+  if [ -n "$peerInfo" ]
+  then
+    echo ",\"${node_id}\":${peerInfo}"
+  #else
+    #TODO check whether we can ping nodes we are not having a channel with
+    #peerInfo=$(lncli connect "$node_id")
+    #echo ",\"${node_id}\":${peerInfo}"
+  fi
 done
 
 echo "}"
