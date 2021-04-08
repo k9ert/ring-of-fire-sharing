fa11d0345ac77ca650753a0528e5929875ca26dd
Lightning Pi <lightning@raspberrypi>
2021-04-08 16:57:44 +0200

    Make status updates recurring every 10 minutes

--- sendRingStats.sh
+++ sendRingStats.sh
@@ -4,4 +4,9 @@ password=$(jq -c '.auth.password' ringOfFireConfig.json | tr -d '"')
 url=$(jq -c '.url' ringOfFireConfig.json | tr -d '"')
 ringStats=$(. getRingStats.sh)
 
-curl -u "$user":"$password"  -X POST -H "Content-Type: application/json" -d "$ringStats"  "$url"/channel/123/balance
+while true
+do
+  echo "Send ring status: $ringStats"
+  curl -u "$user":"$password"  -X POST -H "Content-Type: application/json" -d "$ringStats"  "$url"/channel/123/balance
+  sleep 600
+done
