1074ecd1e8fb3d06f7f54b16f2a939ca8c54f096
Czino <eric@czino.de>
2021-04-09 17:20:37 +0200

    Improve ring data request json

--- getRingStats.sh
+++ getRingStats.sh
@@ -1,21 +1,25 @@
 echo "{"
 
-feeReport=$(lncli feereport)
+my_node_id=$(lncli getinfo | jq -r '.identity_pubkey')
+feeReport=$(lncli feereport | jq -r '.')
 
-echo "\"feeReport\":${feeReport}"
+echo "\"nodeId\":\"${my_node_id}\","
+echo "\"feeReport\":${feeReport},"
 
+echo "\"ringPeers\": ["
 jq -c '.peers[]' ringOfFireConfig.json | while read peer; do
   peer=$(echo "$peer" | tr -d '"')
   node_id=$(echo "$peer" | cut -f1 -d@)
-  peerInfo=$(lncli listchannels --peer "$node_id" | jq -r '{ chan_id: .channels[].chan_id, local_balance: .channels[].local_balance, remote_balance: .channels[].remote_balance, active: .channels[].active }')
+  peerInfo=$(lncli listchannels --peer "$node_id" | jq -r --arg node_id "$node_id" '{ nodeId: $node_id, chan_id: .channels[].chan_id, local_balance: .channels[].local_balance, remote_balance: .channels[].remote_balance, active: .channels[].active }')
   if [ -n "$peerInfo" ]
   then
-    echo ",\"${node_id}\":${peerInfo}"
+    echo "${peerInfo},"
   #else
     #TODO check whether we can ping nodes we are not having a channel with
     #peerInfo=$(lncli connect "$node_id")
-    #echo ",\"${node_id}\":${peerInfo}"
+    #echo "\"${node_id}\":${peerInfo},"
   fi
 done
 
+echo "{}]"
 echo "}"
